4efb5c8d177f306fc343648c06efea062d9528ff
nitinkumar_p <nitinkumar.pawar@arrkgroup.com>
2023-01-28 03:04:59 +0530

    New Update

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,5 +1,6 @@
 from app import app
 from model.user_model import user_model
+from flask import request
 
 obj = user_model()
 
@@ -12,3 +13,9 @@ def user_signup_controller():
 @app.route("/user/getall")
 def user_getall_controller():
     return obj.user_getall_model()
+
+
+@app.route("/user/addone", methods=["POST"])
+def user_addone_controller():
+    # print(request.form)
+    return obj.user_addone_model(request.form)
--- model/user_model.py
+++ model/user_model.py
@@ -1,11 +1,15 @@
+import json
+
 import mysql.connector
 
 
 class user_model():
     def __init__(self):
         try:
-            con = mysql.connector.connect(host="localhost", user="admin", password="Password@123",
-                                          database="flask_demo")
+            self.con = mysql.connector.connect(host="localhost", user="admin", password="Password@123",
+                                               database="flask_demo")
+            self.con.autocommit = True
+            self.cur = self.con.cursor(dictionary=True)
             print("Connection Succesfully established")
         except:
             print("Some error")
@@ -14,4 +18,16 @@ class user_model():
         return "This is user signup model1"
 
     def user_getall_model(self):
-        return "This is user getall model1"
+        self.cur.execute("SELECT * FROM users")
+        result = self.cur.fetchall()
+        if len(result) > 0:
+            return json.dumps(result)
+        else:
+            return "No data found"
+
+    def user_addone_model(self, data):
+        # self.cur.execute("SELECT * FROM users")
+        self.cur.execute(
+            f"INSERT INTO users(name, email, phone ,role ,password) VALUES ('{data['name']}','{data['email']}','{data['phone']}','{data['role']}','{data['password']}')")
+        print(data)
+        return "User created successfully"
